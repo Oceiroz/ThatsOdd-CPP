085ba1f6ed16d61760868ef7495a2dd136161dd8
Lucas <115151663+Oceiroz@users.noreply.github.com>
2024-09-11 13:07:41 +0100

    Add files via upload

---
+++ Thats Odd/Thats Odd.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int RandomNumber();
+
+int main()
+{
+    while (true)
+    {
+        int number = RandomNumber();
+        cout << number << endl;
+        cout << 1 % 2 << endl;
+        string strNum = to_string(number);
+        map<int, int> valueAppearance;
+        int oddCount = 0;
+        int thatsOdd = 0;
+        for (char c : strNum)
+        {
+            string strC = to_string(c);
+            int intC = stoi(strC);
+            valueAppearance[intC]++;
+        }
+        for (const auto value : valueAppearance)
+        {
+            if (value.second % 2 == 1)
+            {
+                oddCount++;
+                thatsOdd = value.first;
+            }
+        }
+        if (oddCount == 1)
+        {
+            cout << thatsOdd - 48 << endl;
+            break;
+        }
+    }
+}
+
+int RandomNumber()
+{
+    srand((unsigned)time(NULL));
+    int randNum = rand();
+    return randNum;
+}
+
